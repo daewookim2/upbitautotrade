58d033d920fc55386e55fd98c468d14b44dda74b
daewookim2 <49084154+daewookim2@users.noreply.github.com>
2021-05-15 11:54:25 +0900

    Update adaAutoTrade.py

--- adaAutoTrade.py
+++ adaAutoTrade.py
@@ -40,20 +40,20 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-ada")
+        start_time = get_start_time("KRW-ADA")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ada", 0.5)
-            current_price = get_current_price("KRW-ada")
+            target_price = get_target_price("KRW-ADA", 0.5)
+            current_price = get_current_price("KRW-ADA")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-ada", krw*0.9995)
+                    upbit.buy_market_order("KRW-ADA", krw*0.9995)
         else:
-            ada = get_balance("ada")
-            if ada > 2.5:
-                upbit.sell_market_order("KRW-ada", ada*0.9995)
+            ADA = get_balance("ADA")
+            if ADA > 2.5:
+                upbit.sell_market_order("KRW-ADA", ADA*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
